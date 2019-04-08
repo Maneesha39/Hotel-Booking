52938b6bc1d524c08d64dab9792f9cc5c4a14c90
Priyankachittibabu <39339421+Priyankachittibabu@users.noreply.github.com>
2019-04-08 12:29:21 +0530

    Initial commit

---
+++ clientSide/src/app/hotel-description/hotel-description.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HotelDescriptionComponent } from './hotel-description.component';
+
+describe('HotelDescriptionComponent', () => {
+  let component: HotelDescriptionComponent;
+  let fixture: ComponentFixture<HotelDescriptionComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HotelDescriptionComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HotelDescriptionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
