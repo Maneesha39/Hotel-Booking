40a09d7a5b839cb61c5adb7f5dd5fb00f0b57c10
Priyankachittibabu <39339421+Priyankachittibabu@users.noreply.github.com>
2019-04-09 14:29:22 +0530

    addhotel

--- clientSide/src/app/app-routing.module.ts
+++ clientSide/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HotelsListComponent } from './hotels-list/hotels-list.component';
+import { AddHotelsComponent } from './add-hotels/add-hotels.component';
 
 const routes: Routes = [
 
@@ -8,7 +9,14 @@ const routes: Routes = [
   {
     path: '',
     component: HotelsListComponent
+  },
+
+  {
+    path: 'hotelsList/addHotels',
+    component: AddHotelsComponent
   }
+
+
 ];
 
 @NgModule({
