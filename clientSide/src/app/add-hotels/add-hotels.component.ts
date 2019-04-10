f683eb596bb445c07402d47a4e7946bee69609d3
venkateswarlu <nallapadu.venkateswarlu@gmail.com>
2019-04-10 19:35:05 +0530

    addhotelform and few changes in db

--- clientSide/src/app/add-hotels/add-hotels.component.ts
+++ clientSide/src/app/add-hotels/add-hotels.component.ts
@@ -10,6 +10,7 @@ import { HotelService } from '../hotel.service';
 })
 export class AddHotelsComponent implements OnInit {
   addHotelForm: FormGroup;
+
   submitted: Boolean = false;
   hotelService: any;
 
@@ -20,14 +21,16 @@ export class AddHotelsComponent implements OnInit {
       name: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(100)]],
       image: ['', [Validators.required]],
       price: ['', [Validators.required, Validators.min(1)]],
-      place: ['', [Validators.required, Validators.min(3)]],
-      landmark: ['', [Validators.required, Validators.min(3)]],
+      place: ['', [Validators.required, Validators.min(3), Validators.max(50)]],
+      landmark: ['', [Validators.required, Validators.min(3), Validators.max(100)]],
       pincode: ['', [Validators.required, Validators.min(6), Validators.max(6)]],
-      mobile: ['', [Validators.required, Validators.min(10), Validators.max(10)]]
+      mobile: ['', [Validators.required, Validators.min(10), Validators.max(10)]],
+      city: ['', [Validators.required, Validators.min(3), Validators.max(30)]]
 
-      // address: ['', [Validators.minLength(11), Validators.maxLength(250)]],
-      // image: ['', [Validators.required]]
     });
+
+
+
   }
   get f() { return this.addHotelForm.controls; }
 
@@ -45,7 +48,7 @@ export class AddHotelsComponent implements OnInit {
   }
 
   navigateTohotelsList() {
-    this.router.navigate(["hotels-list"]);
+    this.router.navigate(["hotel-list"]);
   }
 }
 
