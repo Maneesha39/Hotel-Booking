95d0222ca2f47f23c1448861f448c157388c5edf
Maneesha39 <48672887+Maneesha39@users.noreply.github.com>
2019-04-10 10:15:23 +0530

    UI First page

--- clientSide/materialModule.ts
+++ clientSide/materialModule.ts
@@ -1,11 +1,11 @@
-import {A11yModule} from '@angular/cdk/a11y';
-import {DragDropModule} from '@angular/cdk/drag-drop';
-import {PortalModule} from '@angular/cdk/portal';
-import {ScrollingModule} from '@angular/cdk/scrolling';
-import {CdkStepperModule} from '@angular/cdk/stepper';
-import {CdkTableModule} from '@angular/cdk/table';
-import {CdkTreeModule} from '@angular/cdk/tree';
-import {NgModule} from '@angular/core';
+import { A11yModule } from '@angular/cdk/a11y';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { PortalModule } from '@angular/cdk/portal';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { CdkStepperModule } from '@angular/cdk/stepper';
+import { CdkTableModule } from '@angular/cdk/table';
+import { CdkTreeModule } from '@angular/cdk/tree';
+import { NgModule } from '@angular/core';
 import {
   MatAutocompleteModule,
   MatBadgeModule,
@@ -90,4 +90,5 @@ import {
     ScrollingModule,
   ]
 })
-export class DemoMaterialModule {}
+export class DemoMaterialModule { }
+export class MaterialModule { }
--- clientSide/src/app/app.module.ts
+++ clientSide/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -8,6 +9,10 @@ import { HotelsListComponent } from './hotels-list/hotels-list.component';
 import { HotelDescriptionComponent } from './hotel-description/hotel-description.component';
 import { DemoMaterialModule } from '../../materialModule';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,7 +24,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     DemoMaterialModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
 
   ],
   providers: [],
