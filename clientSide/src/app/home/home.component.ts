e3e6c57c2945601389c8beccb6e99d60d81fd64e
Maneesha39 <48672887+Maneesha39@users.noreply.github.com>
2019-04-12 09:55:15 +0530

    No chnages done

--- clientSide/src/app/home/home.component.ts
+++ clientSide/src/app/home/home.component.ts
@@ -45,44 +45,6 @@ export class HomeComponent implements OnInit {
     document.getElementById("enable").style.display = "block";
   }
 }
-//   done() {
-//     let persons = document.getElementById("details");
-//     persons.style.display = "none";
-//   }
-// }
 
-
-// }
-    // var checkin = $('#dp1').datepicker({
-
-    //   beforeShowDay: function (date) {
-    //     return date.valueOf() >= now.valueOf();
-    //   },
-    //   autoclose: true
-
-    // }).on('changeDate', function (ev) {
-    //   if (ev.date.valueOf() > checkout.datepicker("getDate").valueOf() || !checkout.datepicker("getDate").valueOf()) {
-
-    //     var newDate = new Date(ev.date);
-    //     newDate.setDate(newDate.getDate() + 1);
-    //     checkout.datepicker("update", newDate);
-
-    //   }
-    //   $('#dp2')[0].focus();
-    // });
-
-
-    // var checkout = $('#dp2').datepicker({
-    //   beforeShowDay: function (date) {
-    //     if (!checkin.datepicker("getDate").valueOf()) {
-    //       return date.valueOf() >= new Date().valueOf();
-    //     } else {
-    //       return date.valueOf() > checkin.datepicker("getDate").valueOf();
-    //     }
-    //   },
-    //   autoclose: true
-
-    // }).on('changeDate', function (ev) { });
-// export class DatepickerOverviewExample { }
 
 
